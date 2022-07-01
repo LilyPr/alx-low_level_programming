78c65d06f0f03f59ae8f1de6bc1cb620f7261f89
LilyPr <lilyeginald09@gmail.com>
2022-07-01 06:21:12 -0700

    prints reverse numbers

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,18 +1,18 @@
 #include <stdio.h>
 
 /**
- * main - Prints the lowercase alphabet in reverse.
+ *  main - Prints the lowercase alphabet in reverse.
  *
- * Return: Always 0
+ *  Return: Always 0.
  */
 int main(void)
 {
 	char letter;
 
 	for (letter = 'z'; letter >= 'a'; letter--)
-			putchar(letter);
+		putchar(letter);
 
-			putchar('\n');
+	putchar('\n');
 
-			return (0);
-			}
+	return (0);
+}
