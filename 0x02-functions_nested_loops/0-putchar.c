dc24d50503475d5165f870f8c86e06c65e98360a
LilyPr <lilyeginald09@gmail.com>
2022-07-05 14:15:52 -0700

    all putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -6,15 +6,17 @@
  * @Return - Brief description of return value
  *
  * Return: Describe the return value of function_name.
- * Return: Always return 0
  */
 int main(void)
 {
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
 	_putchar('h');
 	_putchar('a');
-	_putchar('p');
-	_putchar('p');
-	_putchar('y');
+	_putchar('r');
 	_putchar('\n');
 
 	return (0);
