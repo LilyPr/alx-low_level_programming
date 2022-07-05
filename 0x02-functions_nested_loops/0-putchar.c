098420e674590c8bfba2682fca9b5e6ebf5fdae3
LilyPr <lilyeginald09@gmail.com>
2022-07-05 12:25:14 -0700

    all putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -6,18 +6,20 @@
 #include "main.h"
 
 /**
- *  prints _putchar, followed by a new line
- *   program should return 0
+ * main - entry print "putchar", followed by a new line
+ * and program should return 0
+ *   
+ * Return: Always 0(success)
  */
 int main(void)
 {
-	_putchar('w');
-	_putchar('e');
-	_putchar('l');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
 	_putchar('c');
-	_putchar('o');
-	_putchar('m');
-	_putchar('e');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
 	_putchar('\n');
 
 	return (0);
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -2,9 +2,11 @@
 #define MAIN_H
 
 /**
- *  Desc: Header file containing declarations for all functions
- *   And the prototype of the function 
+ * Desc: Header file containing declarations for all functions
+ * And the prototype of the function 
+ *  used in the 0x02-functions_nested_loops directory
  */
+
 int _putchar(char c);
 int main(void);
 
