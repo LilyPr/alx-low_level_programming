7c9a2e62183eff33c9c18a081b380e796d031683
LilyPr <lilyeginald09@gmail.com>
2022-07-05 00:03:55 -0700

    print putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+/*
+ * This is my my File 0-putchar
+ * Written: Lilian Mihanjo
+ *
+ */
+
+#include "main.h"
+
+/*
+ * main - prints _putchar, followed by a new line.
+ *
+ * Return always 0 for successful
+ */
+int main(void)
+{
+_putchar('my file');
+_putchar('main');
+_putchar('\n');
+
+return (0);
+}
