1a111015902755d5a32f0f0dc7b86f6964ca7b68
LilyPr <lilyeginald09@gmail.com>
2022-07-05 22:06:55 -0700

    100fibonacci

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -6,6 +6,7 @@
  *
  * Return: Always 0.
  */
+
 int main(void)
 {
 	int count;
@@ -21,7 +22,6 @@ int main(void)
 		fib1 = fib2;
 		fib2 = sum;
 	}
-
 	fib1_half1 = fib1 / 10000000000;
 	fib2_half1 = fib2 / 10000000000;
 	fib1_half2 = fib1 % 10000000000;
@@ -47,6 +47,7 @@ int main(void)
 		fib2_half2 = half2;
 	}
 	printf("\n");
+	
 	return (0);
 }
 
