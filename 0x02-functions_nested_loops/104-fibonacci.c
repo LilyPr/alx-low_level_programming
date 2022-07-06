d4c56e2ee33160c48ae11718224690ec6b535e3c
LilyPr <lilyeginald09@gmail.com>
2022-07-06 00:18:17 -0700

    104fibonacci

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -15,7 +15,7 @@ unsigned long half1, half2;
 for (count = 0; count < 92; count++)
 {
 sum = fib1 + fib2;
-printf("%lu, ", sum);
+printf("%lu,", sum);
 fib1 = fib2;
 fib2 = sum;
 }
@@ -41,5 +41,6 @@ fib2_half1 = half1;
 fib2_half2 = half2;
 }
 printf("\n");
+
 return (0);
 }
