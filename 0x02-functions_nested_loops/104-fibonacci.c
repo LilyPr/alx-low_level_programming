394262a74e62edd2cb27d3da02aa49d18f861abc
LilyPr <lilyeginald09@gmail.com>
2022-07-05 21:54:41 -0700

    print 100 fibonacci

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -20,7 +20,6 @@ int main(void)
 
 		fib1 = fib2;
 		fib2 = sum;
-
 	}
 
 	fib1_half1 = fib1 / 10000000000;
@@ -30,14 +29,14 @@ int main(void)
 
 	for (count = 93; count < 99; count++)
 	{
-		half1 = fib1_half1 + fib2_half1;
+		half1 = fib1_half1 + fib2_half;
 		half2 = fib1_half2 + fib2_half2;
 		if (fib1_half2 + fib2_half2 > 9999999999)
 		{
 			half1 += 1;
 			half2 %= 10000000000;
 		}
-
+		
 		printf("%lu%lu", half1, half2);
 		if (count != 98)
 			printf(", ");
@@ -46,9 +45,9 @@ int main(void)
 		fib1_half2 = fib2_half2;
 		fib2_half1 = half1;
 		fib2_half2 = half2;
-
 	}
-	
 	printf("\n");
 	return (0);
 }
+
+	
