c30faf3101fc6f5f12065e50041aa83de78325eb
LilyPr <lilyeginald09@gmial.com>
2022-07-07 11:37:07 -0700

    largest three numbers

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -12,17 +12,26 @@ int largest_number(int a, int b, int c)
 {
 	int largest;
 
+	printf(" a = 972");
+	scanf("%d", &a);
+
+	printf(" b = -98");
+	scanf("%d", &b);
+
+	printf(" b = -98");
+	scanf("%d", &c);
+
 	if (a > b && b > c)
 	{
-		largest = a;
+		printf("%d is the largest number\n", a);
 	}
 	else if (b > a && a > c)
 	{
-		largest = b;
+		printf("%d is the largest number\n", b);
 	}
 	else
 	{
-		largest = c;
+		printf("%d is the largest number\n", c);
 	}
 
 	return (largest);
