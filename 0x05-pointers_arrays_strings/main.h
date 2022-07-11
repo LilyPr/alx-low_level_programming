bbc55a5c2c0483f4758cfed102aec986d6c538cc
LilyPr <lilyeginald09@gmial.com>
2022-07-11 01:29:17 -0700

    a pointer and update value of it

---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,14 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * File: Main.h
+ * Desc: Header file containing prototypes for all functions
+ * used in directory.
+ */
+
+#include <stdlib.h>
+
+void reset_to_98(int *n);
+
+#endif
