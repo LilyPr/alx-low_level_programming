2075841c31bdd662f84f7142b6e77ce5d91eff19
LilyPr <lilyeginald09@gmail.com>
2022-07-12 09:46:43 +0300

    print reverse

--- 0x05-pointers_arrays_strings/4-main.c
+++ 0x05-pointers_arrays_strings/4-main.c
@@ -9,7 +9,7 @@ int main(void)
 {
     char *str;
 
-    str = "I do not fear computers. I fear the lack of them - Isaac Asimov";
+    str = "hey software engineering so cool..!";
     print_rev(str);
     return (0);
 }
