497341fff9574e6504433ec2b8928e878bb78975
LilyPr <lilyeginald09@gmail.com>
2022-07-14 05:42:10 -0700

    add one line

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
 
-/** main - add one line to this code, so that the program prints a[2] = 98*/
-
+/**
+ * main - add one line to this code so that the program prints a[2] = 98
+ *
+ * Return: Always 0
+ */
 int main(void)
 {
 	int n;
@@ -11,13 +14,10 @@ int main(void)
 	a[2] = 1024;
 	p = &n;
 	/*
-	 * write your line of code here...
-	 * Remember:
-	 * - you are not allowed to use a
-	 * - you are not allowed to modify
+	 * you are not allowed to use a or modify p
 	 */
 	*(p + 5) = 98;
-	/* ...so that this prints 98\n */
+	 /* ...so that this prints 98\n */
 	printf("a[2] = %d\n", a[2]);
 	return (0);
 }
