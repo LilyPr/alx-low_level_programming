0a998c382ea5826743de295a5f900d529c16bbde
LilyPr <lilyeginald09@gmail.com>
2022-07-14 06:12:34 -0700

    add one line

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -7,13 +7,13 @@
  */
 int main(void)
 {
+	int n;
+	int a[2];
+	int *p;
 
-	
-	
-	
-	
-	
-	
+	a[2] = 1024;
+	p = &n;
+	*(p + 2) = 98;
 	
 	/* this is code display output*/
 	printf("a[2] = %d\n", a[2]);
