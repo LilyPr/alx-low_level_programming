4a8961f95097f4ccd60d97c763fdc6eebf165c56
LilyPr <lilyeginald09@gmail.com>
2022-07-14 06:06:30 -0700

    add one line

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -7,17 +7,15 @@
  */
 int main(void)
 {
-	int n;
-	int a[2];
-	int *p;
 
-	a[2] = 1024;
-	p = &n;
-	/*
-	 * you are not allowed to use a or modify p
-	 */
-	*(p + 2) = 98;
-	 /* ...so that this prints 98\n */
+	
+	
+	
+	
+	
+	
+	
+	/* this is code display output*/
 	printf("a[2] = %d\n", a[2]);
 	return (0);
 }
