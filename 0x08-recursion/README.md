f41875a79f6599ee4440092b04357846ee6f895c
LilyPr <lilyeginald09@gmail.com>
2022-07-20 03:24:23 -0700

    prints a string, followed by a new line

---
+++ 0x08-recursion/README.md
@@ -0,0 +1,2 @@
+Description of recursion of how it work, do and use in c programming 
+so through this 0x08-recursion directory I will learn and know to use recursion
---
+++ 0x08-recursion/main.h
@@ -0,0 +1,12 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * Desc: Header file containing prototypes for all functions
+ *       used in the 0x08-recursion directory.
+ */
+
+int _putchar(char c);
+void _puts_recursion(char *s);
+
+#endif
