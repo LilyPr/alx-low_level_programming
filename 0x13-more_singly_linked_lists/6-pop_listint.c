a2a41c188aeb5f3fc5f5174ef8b7a3043bee1581
LilyPr <lilyeginald09@gmail.com>
2022-08-16 07:40:06 -0700

    deletes the head node

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -10,14 +10,13 @@
 int pop_listint(listint_t **head)
 {
 	listint_t *new_head;
-	int n = 0;
+	int n;
 
-	if (*head != NULL)
-	{
-		new_head = (*head)->next;
-		n = (*head)->n;
-		free(*head);
-		*head = new_head;
-	}
+	if (*head == NULL)
+		return (0);
+	new_head = *head;
+	new_head = (*head)->next;
+	n = (*head)->n;
+	free(new_head);
 	return (n);
 }
