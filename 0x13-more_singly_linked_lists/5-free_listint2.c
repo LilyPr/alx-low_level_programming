cb07018e4234d7ad988a99f14ec6c5da55ca92fa
LilyPr <lilyeginald09@gmail.com>
2022-08-16 07:08:27 -0700

    frees

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -8,21 +8,15 @@
 
 void free_listint2(listint_t **head)
 {
-	listint_t *temp;
+	listint_t *tmp;
 
-	if (head)
-	{
-		while (*head)
-		{
-			temp = (*head);
-			*head = (*head)->next;
-			free(temp);
-		}
-	}
-	else
-	{
-		return;
-	}
-	free(*head);
-	head = 0;
+if (head == NULL)
+return;
+
+while (*head != NULL)
+{
+tmp = *head;
+*head = tmp->next;
+free(tmp);
+}
 }
