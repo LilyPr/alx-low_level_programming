6655b42212e3d1eee800ea827768c8276c2f70dd
LilyPr <lilyeginald09@gmil.com>
2022-12-08 22:11:48 +0300

    change files

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,31 +1,22 @@
 #!/usr/bin/python3
+"""Island perimeter program"""
 
 
 def island_perimeter(grid):
-    """ Function that returns detail of a grid"""
+    """Returns the perimeter from a grid"""
+    if len(grid) == 0 or len(grid[0]) == 0:
+        return 0
 
-    peri = 0;
-    for i in range(len(grid)):
-        for j in range(len(grid[0])):
-            if grid[i][j] == 1:
-                try:
-                    if grid[i - 1][j] == 0 or i == 0:
-                        peri += 1
-                except IndexError:
-                    peri += 1
-                    try:
-                        if grid[i + 1][j] == 0:
-                            peri += 1
-                    except IndexError:
-                        peri += 1
-                        try:
-                            if grid[i][j - 1] == 0 or j == 0:
-                                peri += 1
-                        except IndexError:
-                            peri += 1
-                            try:
-                                if grid[i][j + 1] == 0:
-                                    peri += 1
-                            except IndexError:
-                                peri += 1
-return (peri)
+    rows_len = len(grid)
+    column_len = len(grid[0])
+    row_set = set()
+    col_set = set()
+
+    for row in range(rows_len):
+        for col in range(column_len):
+            if grid[row][col] == 1:
+                row_set.add(row)
+                col_set.add(col)
+
+    total = (len(row_set) * 2) + (len(col_set) * 2)
+    return total
